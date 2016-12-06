82a14bb0c1d327958070ded9e9016cba95d37af4
Roman <romanrimskiy@gmail.com>
2016-12-06 23:54:29 +0200

    create router

---
+++ lib/router.class.php
@@ -0,0 +1,46 @@
+<?php
+
+
+class Router
+{
+    protected  $uri;
+    protected $controller;
+    protected $action;
+    protected  $params;
+
+
+    public function __construct( $uri )
+    {
+        $this->uri = urldecode( trim( $uri,'/' ) );
+
+        //Set Defaults
+        $this->action='index';
+
+        //Get path like /lng/controllers/action/param1/param2/.../...
+
+        $path=$this->uri;
+        $path_parts=explode( '/',$path );
+
+        if ($path_parts){
+
+            //Get  controller
+            if ( current( $path_parts )){
+                $this->controller=strtolower(current( $path_parts ));
+                array_shift($path_parts);
+            }
+
+            //Get  action
+            if ( current( $path_parts )){
+                $this->action=strtolower(current( $path_parts ));
+                array_shift($path_parts);
+            }
+
+            //Get params
+            $this->params=$path_parts;
+
+        }
+
+    }
+
+
+}
