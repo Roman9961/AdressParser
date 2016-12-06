ed1f6680d5fdc7622eeddb3e49b7f5c9dc6889ec
Roman <romanrimskiy@gmail.com>
2016-12-06 23:05:48 +0200

    some improve of catalogue structure

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -1,15 +1,22 @@
 <?php
 spl_autoload(function($class_name)
 {
-    $controllers_path=ROOT.DS.'controllers'.DS.str_replace('controller', '', strtolower($class_name)).'.controller.php';
-    $model_path=ROOT.DS.'models'.DS.strtolower($class_name).'.php';
+    $lib_path = ROOT.DS.'lib'.DS.strtolower($class_name).'.class.php';
+    $controllers_path = ROOT.DS.'controllers'.DS.str_replace('controller', '', strtolower($class_name)).'.controller.php';
+    $model_path = ROOT.DS.'models'.DS.strtolower($class_name).'.php';
 
-    if (file_exists($controllers_path)){
+    if (file_exists($lib_path)){
+        require_once ($lib_path);
+    }
+
+    elseif (file_exists($controllers_path)){
         require_once ($controllers_path);
     }
+
     elseif (file_exists($model_path)){
         require_once ($model_path);
     }
+
     else{
         throw new Exception('Failed to include class: '. $class_name);
     }
--- lib/controller.class.php
+++ lib/controller.class.php
@@ -1,12 +1,35 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: Roman&Kate
- * Date: 12/6/2016
- * Time: 10:54 PM
- */
+
 class Controller
 {
+    protected $data;
+    protected $model;
+    protected $params;
+
+
+    public function __construct($data=array())
+    {
+        $this->data = $data;
+    }
+
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+
+    public function getParams()
+    {
+        return $this->params;
+    }
+
 
 }
